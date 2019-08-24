56a293808917b75b15159e12663df4e2dc268305
gowrishgit <reach.gowrish@gmail.com>
2019-08-24 03:23:06 -0400

    new

---
+++ Mavenjava - Copy/src/test/java/Seleniumtraining/SeleniumTest.java
@@ -0,0 +1,18 @@
+package Seleniumtraining;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+@Test
+
+public void BroswerAutomation()
+{
+System.out.println("BroswerAutomation");	
+}
+@Test
+public void elementUI()
+{
+System.out.println("elementUI");	
+}
+
+}
